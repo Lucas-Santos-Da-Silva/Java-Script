7b35b8e678667dddf9fa88577d1cc2fa89a83a37
Lucas Santos da Silva <lucassantos.ypo@gmail.com>
2024-05-15 14:19:20 -0300

    exercicios poo e modificação tutorial.txt

---
+++ exercicios/calculo tijolo por m2/calculadora.js
@@ -0,0 +1,12 @@
+/**
+ * Calcular tijolos por metro quadrado
+ * @author Lucas Santos da Silva
+ */
+
+let altura, comprimento, metro_quadrado, rs
+
+function calcular(){
+    altura = Number(frmFlex.txtnota1.value)
+    comprimento = Number(frmFlex.txtnota2.value)
+    altura = Number(frmFlex.txtnota1.value)
+}
