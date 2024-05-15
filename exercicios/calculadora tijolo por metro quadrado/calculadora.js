94ee5d731528f6a896c6973855ce866a4dcb704c
Lucas Santos Da Silva <84152031+Lucas-Santos-Da-Silva@users.noreply.github.com>
2024-05-15 01:23:53 -0300

    Add files via upload

---
+++ exercicios/calculadora tijolo por metro quadrado/calculadora.js
@@ -0,0 +1,21 @@
+/**
+ * Calcular tijolos por metro quadrado
+ * @author Lucas Santos da Silva
+ */
+
+let metro_quadrado, rs, rs2
+altura = 0.115
+comprimento = 0.24
+um_metro = 1
+
+function calcular(){
+
+    metro_quadrado = Number(frmFlex.input_metro_quadrado.value)
+
+    
+    rs2 = um_metro/altura/comprimento
+    rs = metro_quadrado/altura/comprimento
+    
+    frmFlex.input_resultado1.value = rs2.toFixed(1) 
+    frmFlex.input_resultado2.value = rs.toFixed(1)
+}
